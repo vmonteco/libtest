42b1a3587cbcf673d21a96d1bb18825d9436f22a
Vincent MONTÉCOT <vincent.montecot@hotmail.fr>
2024-03-05 00:02:03 +0100

    added print_main_result

---
+++ src/print_main_result.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_main_result.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmonteco </var/spool/mail/vmonteco>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 23:55:07 by vmonteco          #+#    #+#             */
+/*   Updated: 2024/03/04 23:58:18 by vmonteco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+
+extern int		tests_success;
+extern int		tests_ran;
+
+void	print_main_result()
+{
+	printf(
+		"Final result : [%s% 2d/% 2d %s]\n",
+		((tests_success == tests_ran) ? COLOR_GREEN : COLOR_RED),
+		tests_success,
+		tests_ran,
+		COLOR_RESET
+	);
+}
